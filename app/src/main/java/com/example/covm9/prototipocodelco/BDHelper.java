aad96c1afb96f34f5cbc5fd44d5f017a47430d38
estefy <estefania,f@hotmail.es>
2016-01-29 12:12:34 -0300

    metodos de bd

--- app/src/main/java/com/example/covm9/prototipocodelco/BDHelper.java
+++ app/src/main/java/com/example/covm9/prototipocodelco/BDHelper.java
@@ -2,6 +2,7 @@ package com.example.covm9.prototipocodelco;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -15,10 +16,27 @@ public class BDHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+
+        //se crea la tabla Divisiones
+        db.execSQL("create table divisio(codigoDivision text primary key," +
+                "nombre text)");
+
+        //se crea la tabla Area
+        db.execSQL("create table area(codigoArea text primary key," +
+                "nombreArea text," +
+                "descripcionArea text,"+
+                "FOREIGN KEY (codigoDivision) REFERENCES division (codigoDivision)");
+
         //se crea la tabla Fichas
-        db.execSQL("create table fichas(id text primary key," +
+        db.execSQL("create table fichas(idFicha text primary key," +
                 "nombre text," +
-                "codigoQR int)");
+                "DescripcionFicha text," +
+                "link text,"+
+                "FOREIGN KEY (codigoQr) REFERENCES qr (codigoQr))");
+
+        //se crea la tabla QR
+        db.execSQL("create table qr(codigoQr text primary key," +
+                "FOREIGN KEY (codigoArea) REFERENCES area (codigoArea))");
     }
 
     @Override
@@ -26,18 +44,40 @@ public class BDHelper extends SQLiteOpenHelper {
 
     }
 
-    public void cargarFichas(String id, String nombre, int codigoQR,SQLiteDatabase bd){
-
-        //se crea un registro para insertar datos
-        ContentValues registro = new ContentValues();
-        //se agregan los datos del lugar al registro
-        registro.put("id", id);
-        registro.put("nombre", nombre);
-        registro.put("codigoQR", codigoQR);
-
-        //se agrega el lugar a la bd
-        bd.insert("ubicacion", null, registro);
-
-
+    public void actualizarQr(SQLiteDatabase db){
+        db.execSQL("DROP TABLE qr");
+        //se crea la tabla QR
+        db.execSQL("create table qr(codigoQr text primary key," +
+                "FOREIGN KEY (codigoArea) REFERENCES area (codigoArea))");
     }
+
+    public void actualizarFicha(SQLiteDatabase db){
+        db.execSQL("DROP TABLE ficha");
+        //se crea la tabla Fichas
+        db.execSQL("create table fichas(idFicha text primary key," +
+                "nombre text," +
+                "DescripcionFicha text," +
+                "link text,"+
+                "FOREIGN KEY (codigoQr) REFERENCES qr (codigoQr))");
+    }
+    public String buscarFicha (SQLiteDatabase db, String codigoQr, int posicion ){
+
+        Cursor fila = db.rawQuery("select link from ficha where codigoQr= '" + codigoQr + "'", null);
+        String link = fila.getString(posicion);
+        return  link;
+    }
+
+    public String buscarDivision (SQLiteDatabase db, String nombreDivision){
+
+        Cursor fila = db.rawQuery("select codigoDivision from division where nombreDivision= '" + nombreDivision + "'", null);
+        String codigo = fila.getString(0);
+        return  codigo;
+    }
+
+    public Cursor buscarArea (SQLiteDatabase db, String codigoDivision){
+
+        Cursor fila = db.rawQuery("select * from area where codigoDivision= '" + codigoDivision + "'", null);
+        return  fila;
+    }
+
 }
